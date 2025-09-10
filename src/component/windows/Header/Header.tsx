48c5309e61833fd91aad198d7b1432b1ee610f80
Durondil <aelozweb@gmail.com>
2025-09-10 18:20:58 +0200

    add window control features: minimize, maximize, close, and toggle maximize

--- src/component/windows/Header/Header.tsx
+++ src/component/windows/Header/Header.tsx
@@ -1,15 +1,43 @@
 import './Header.css'
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 export default function Header() {
+    const handleMinimize = async () => {
+            const win = getCurrentWindow();
+            await win.minimize();
+        }
+    const handleMaximize = async () => {
+            const win = getCurrentWindow();
+            await win.toggleMaximize();
+        }
+    const handleClose = async () => {
+            const win = getCurrentWindow();
+            await win.close();
+        }
     return (
-        <header className="header">
+        <header
+            onDoubleClick={handleMaximize}
+            className="header"
+        >
             <div className="window-title">
                 <span className="window-title-font">Titre de la fenêtre</span>
             </div>
             <div className="window-controls">
-                <button className="window-controls-button hover:bg-gray-600 bg-gray-800" aria-label="Minimiser">_</button>
-                <button className="window-controls-button hover:bg-gray-600 bg-gray-800" aria-label="Maximiser">▢</button>
-                <button className="window-controls-button hover:bg-gray-600 bg-gray-800" aria-label="Fermer">×</button>
+                <button
+                    onClick={handleMinimize}
+                    className="window-controls-button hover:bg-gray-600 bg-gray-800"
+                    aria-label="Minimiser"
+                >_</button>
+                <button
+                    onClick={handleMaximize}
+                    className="window-controls-button hover:bg-gray-600 bg-gray-800"
+                    aria-label="Maximiser"
+                >▢</button>
+                <button
+                    onClick={handleClose}
+                    className="window-controls-button hover:bg-gray-600 bg-gray-800"
+                    aria-label="Fermer"
+                >×</button>
             </div>
         </header>
     )
